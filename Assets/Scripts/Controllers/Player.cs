940c6a55ea0f2abb5a93b5f37ef9a7b81f4fd9e7
Casiana <150386031+casiana-atki@users.noreply.github.com>
2024-09-23 12:04:16 -0400

    Update Player.cs (PlayerMovement)
    Added 8 directional movement.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,34 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public Vector3 velocity = new Vector3(0.01f, 0);
+    public Vector3 height = new Vector3(0, 0.01f);
+
     void Update()
     {
+        //Simplifying our previous statement. The player's position is getting assigned and added to by the velocity vector which is declared as a variable beforehand. 
+        
+        playerMovement();
+    }
 
+    private void playerMovement()
+    {
+        if (Input.GetKey(KeyCode.W)) //Up
+        {
+            transform.position += height;
+        }
+        if (Input.GetKey(KeyCode.A)) //Left
+        {
+            transform.position -= velocity;
+        }
+        if (Input.GetKey(KeyCode.S)) //Down
+        {
+            transform.position -= height;
+        }
+        if (Input.GetKey(KeyCode.D)) //Right
+        {
+            transform.position += velocity;
+        }
     }
 
 }
