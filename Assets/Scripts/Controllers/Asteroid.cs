c000f5cc543f95b3d6d97577e272a8769d70927b
Casiana <atkicasi@sheridancollege.ca>
2024-09-30 20:43:21 -0400

    Task 3 - Asteroids Complete

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -7,14 +7,35 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    private Vector3 targetPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
+        ChooseNewTarget();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MoveTowardsTarget();
+    }
+
+    private void ChooseNewTarget()
+    {
+        float randomX = Random.Range(-maxFloatDistance, maxFloatDistance);
+        float randomY = Random.Range(-maxFloatDistance, maxFloatDistance);
+        targetPosition = transform.position + new Vector3(randomX, randomY, 0);
+    }
+
+    private void MoveTowardsTarget()
+    {
+        if (Vector3.Distance(transform.position, targetPosition) <= arrivalDistance)
+        {
+            ChooseNewTarget();
+        }
+        else
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
     }
 }
